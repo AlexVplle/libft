bab03b12ffbcecdfa516ff53f8f95e02f66c44dd
AlexVplle <alexisvapaille11@gmail.com>
2022-08-21 11:44:42 +0200

    suite

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,20 +6,34 @@
 /*   By: avapaill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 17:10:07 by avapaill          #+#    #+#             */
-/*   Updated: 2022/08/20 17:16:35 by avapaill         ###   ########.fr       */
+/*   Updated: 2022/08/21 11:39:38 by avapaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
-	
-	i = 0;
-	while (s[i])
+	char	*str;
+
+	str = (char *) s;
+	while (*str)
 	{
-		if (s[i] == c)
-			return (s);
-		i++;
+		if (*str == c)
+			return (str);
+		str++;
 	}
+	if (*str == c)
+		return (str);
 	return ((void *) 0);
+}
+
+int main (int argc, char *argv[])
+{
+	if (argc == 3) {
+		//printf("%s", strchr(argv[1], argv[2][0]));
+		printf("%s", ft_strchr(argv[1], argv[2][0]));
+	}
+	return 0;
 }
